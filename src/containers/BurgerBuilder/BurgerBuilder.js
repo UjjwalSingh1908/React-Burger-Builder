ebdb3d9543d6b38418d96d2142d1784fe9938622
UjjwalSingh1908 <ujjwal1911044@akgec.ac.in>
2020-09-14 01:15:11 +0530

    added build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
-    }
+    };
 
     render() {
         return(
          <Aux>
-             <Burger ingredients = {this.state.ingredients} />
-             <div>Build Controls</div>
+            <Burger ingredients = {this.state.ingredients} />
+            <BuildControls /> 
          </Aux>
         );
     }
